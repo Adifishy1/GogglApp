5aeabb755ecbd90cc6d4dcf37610f49dc07e9661
Adifishy1 <aditya16bhor@gmail.com>
2023-10-14 09:20:10 +0530

    first commit

---
+++ src/components/Routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { Results } from './Results';
+
+export const Routing = () => (
+  <div className="p-4">
+    <Routes>
+      <Route exact path="/">
+        <Navigate to="/search" />
+      </Route>
+      <Route exact path="/search">
+        <Results />
+      </Route>
+      <Route path="/images">
+        <Results />
+      </Route>
+      <Route path="/news">
+        <Results />
+      </Route>
+      <Route path="/videos">
+        <Results />
+      </Route>
+    </Routes>
+  </div>
+
+);
